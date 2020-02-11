0d947dc05f12132088fb68c6f2015027d282d531
Furkan Gözükara <furkangozukara@gmail.com>
2020-02-11 13:47:47 +0300

    week 3 v2

--- program codes/week 3 database connect/week 3 database connect/MainWindow.xaml.cs
+++ program codes/week 3 database connect/week 3 database connect/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,69 @@ namespace week_3_database_connect
         public MainWindow()
         {
             InitializeComponent();
+
+            initComboBox();
+        }
+
+        private void initComboBox()
+        {
+            List<csUserRanks> lstUsers = new List<csUserRanks>
+            {
+                new csUserRanks { irRank = -1, srRankTitle = "Select Rank" },
+                new csUserRanks { irRank = 1, srRankTitle = "Admin" },
+                new csUserRanks { irRank = 2, srRankTitle = "Teacher" },
+                new csUserRanks { irRank = 3, srRankTitle = "Student" }
+            };
+
+            cmbBoxUserRank.ItemsSource = lstUsers;
+            cmbBoxUserRank.DisplayMemberPath = "srRankTitle";
+            cmbBoxUserRank.SelectedValuePath = "irRank";
+            cmbBoxUserRank.SelectedIndex = 0;
+        }
+
+        public class csUserRanks
+        {
+            public int irRank { get; set; }
+            public string srRankTitle { get; set; }
+        }
+
+        private static string srConnectionString =
+            "server=localhost;database=okul;Integrated Security=SSPI;Connection Timeout=3000;";
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            int irSelectedRank = (int) cmbBoxUserRank.SelectedValue;
+
+            //string srUserHashedPassword = sha256(pwUserPassword.Password.ToString());
+
+
+
+            //string srQuery=$@"  insert into tblUsers (user_email,user_password,name,surname,user_rank)
+            //    values ('{txtEmail.Text}',
+            //    '{srUserHashedPassword}',
+            //        '{txtFirstName.Text}',
+            //    '{txtSurname.Text}',
+            //    )"
+
+            ////you have to either use using statement or explicity close sql connection
+            //using (SqlConnection connection = new SqlConnection(srConnectionString))
+            //{
+            //    connection.Open();
+            //    SqlCommand command = new SqlCommand(srQuery, connection);
+            //    Message = " rows affected " + command.ExecuteNonQuery();
+            //}
+        }
+
+        static string sha256(string randomString)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
