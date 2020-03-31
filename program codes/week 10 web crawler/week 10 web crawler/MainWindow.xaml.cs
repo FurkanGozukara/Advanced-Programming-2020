2414cd8beb6aa043cbeb8659f306e13a5427a632
Furkan Gözükara <furkangozukara@gmail.com>
2020-03-31 21:38:25 +0300

    week 10 final

---
+++ program codes/week 10 web crawler/week 10 web crawler/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace week_10_web_crawler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ThreadPool.SetMaxThreads(100000, 100000);
+            ThreadPool.SetMinThreads(100000, 100000);
+        }
+
+        private void btnSingleUrlCrawl_Click(object sender, RoutedEventArgs e)
+        {
+            doSingleCrawl(txtUrl.Text);
+        }
+
+        private void doSingleCrawl(string urlAddress)
+        {
+            //  System.Threading.Thread.Sleep(10 * 1000);
+
+            var vrResult = page_fetcher.fetch_a_page(urlAddress);
+
+            if (vrResult.fetchStatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show((int)vrResult.fetchStatusCode + "\n" + vrResult.fetchStatusCode.ToString() +
+                    "\n" +
+                    vrResult.fetchStatusDescription+"\n"+
+                    vrResult.exceptionE.Message);
+                return;
+            }
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtSourceCode.Text = vrResult.srFetchSource;
+            }));
+        }
+
+        private void btnTimerStart_Click(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(update_timer_label);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            dispatcherTimer.Start();
+        }
+
+        private void update_timer_label(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                lblTimer.Content = $"time: {DateTime.Now.ToString()} : {DateTime.Now.Millisecond}";
+            }));
+        }
+
+        private void btnTaskBasedCrawl_Click(object sender, RoutedEventArgs e)
+        {
+            string srUrl = txtUrl.Text;
+            Task vrStartedTask = Task.Factory.StartNew(() => { doSingleCrawl(srUrl); }).ContinueWith(task =>
+            {
+                MessageBox.Show("task has finished: " + task.Status+" "+DateTime.Now);
+            });
+            // vrStartedTask.Wait();
+            MessageBox.Show("task started but not finished yet: " + vrStartedTask.Status + " " + DateTime.Now);
+        }
+
+        private void threadCountTest_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Task.Factory.StartNew(() => { tempTask(); }, TaskCreationOptions.LongRunning);
+            }
+        }
+
+        private void threadCountTestDefault_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Task.Factory.StartNew(() => { tempTask(); });
+            }
+        }
+
+        private void tempTask()
+        {
+            System.Threading.Thread.Sleep(100 * 1000);
+        }
+    }
+}
---
+++ program codes/week 10 web crawler/week 10 web crawler/page_fetcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace week_10_web_crawler
+{
+    public class cs_fetch_result
+    {
+        public string srFetchSource { get; set; }
+        public HttpStatusCode fetchStatusCode { get; set; }
+        public string fetchStatusDescription { get; set; }
+        public Exception exceptionE = null;
+    }
+
+    public static class page_fetcher
+    {
+        public static cs_fetch_result fetch_a_page(string urlAddress)
+        {
+            cs_fetch_result temp_cs_fetch_result = new cs_fetch_result();
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream receiveStream = response.GetResponseStream())
+                    {
+                        StreamReader readStream = null;
+
+                        if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                            readStream = new StreamReader(receiveStream);
+                        else
+                            readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                        temp_cs_fetch_result.srFetchSource = readStream.ReadToEnd();
+
+                        temp_cs_fetch_result.fetchStatusCode = response.StatusCode;
+
+                        readStream.Close();
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                temp_cs_fetch_result.exceptionE = e;
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    temp_cs_fetch_result.fetchStatusCode = ((HttpWebResponse)e.Response).StatusCode;
+                    temp_cs_fetch_result.fetchStatusDescription = ((HttpWebResponse)e.Response).StatusDescription;
+                }
+            }
+            catch (Exception e)
+            {
+                temp_cs_fetch_result.exceptionE = e;
+            }
+
+            return temp_cs_fetch_result;
+        }
+    }
+}
